b97201acf79e84c445f536a2a5e1b31800a08e2f
Rodrigo Martins Pagliares <pagliares@bcc.unifal-mg.edu.br>
2024-10-24 20:21:21 -0300

    feat: implement TUS-1342

---
+++ Release_01/Sprint_05/TUS_1342/src/main/java/com/example/Tus1342Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tus1342Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tus1342Application.class, args);
+	}
+
+}
