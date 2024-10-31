10393d5afa3b1b34e30d7478b3f7b7ae3ee39a9f
Rodrigo Martins Pagliares <pagliares@bcc.unifal-mg.edu.br>
2024-10-31 12:10:42 -0300

    feat: implement TUS-1915

---
+++ Release_01/Sprint_07/TUS-1915/src/main/java/com/example/Tus1915Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tus1915Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tus1915Application.class, args);
+	}
+
+}
